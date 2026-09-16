a9d7618d0e8548838be419cef985645c5bb62ede
naufalilham-dev <naufalilhamfauzan@gmail.com>
2026-09-16 11:42:18 +0700

    Perbaikan tugas

--- bolean.py
+++ bolean.py
@@ -1,11 +1,2 @@
-nilai = input("Masukkan nilai boolean (1/0): ").strip()
-
-if nilai == "1":
-	x = True
-elif nilai == "0":
-	x = False
-else:
-	print("Input harus 1 atau 0.")
-	raise SystemExit
-
-print("Nilai x:", x)
+status_menikah = bool(input("Apakah Anda sudah menikah? (1/0): ") == '1')
+print(status_menikah)
