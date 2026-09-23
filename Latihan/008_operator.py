21066f83e5ac695ea85d969157327bd37ef02652
naufalilham-dev <naufalilhamfauzan@gmail.com>
2026-09-23 12:50:20 +0700

    Pindahan -- Folder

---
+++ Latihan/008_operator.py
@@ -0,0 +1,96 @@
+print("\n=========================================")
+print("ARITMATIKA")
+print("=========================================")
+
+x = 10
+y = 15
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)
+
+print("\n=========================================")
+print("PENUGASAN")
+print("=========================================")
+
+n = 10
+print("Nilai n = ", n)
+
+n += 5
+print("Nilai n = ", n)
+n -= 5
+print("Nilai n = ", n)
+n *= 5
+print("Nilai n = ", n)
+n /= 5
+print("Nilai n = ", n)
+n **= 2
+print("Nilai n = ", n)
+n //= 2
+print("Nilai n = ", n)
+n %= 3
+print("Nilai n = ", n)
+
+print("\n=========================================")
+print("PERBANDINGAN")
+print("=========================================")
+
+a = 10
+b = 15
+
+print("Nilai a = ", a)
+print("Nilai b = ", a)
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a >= b, "\n")
+
+nama1 = "nopal"
+nama2 = "ilham"
+nama3 = "nopal"
+
+print("Nama 1 = ", nama1)
+print("Nama 2 = ", nama2)
+print("Nama 3 = ", nama3)
+print(nama1 == nama2)
+print(nama1 == nama3)
+print(nama2 == nama3)
+print(nama1 != nama2)
+print(nama1 != nama3)
+print(nama2 != nama3)
+
+print("\n=========================================")
+print("LOGIKA")
+print("=========================================")
+
+umur = 20
+print(umur > 15 and umur < 25)
+
+tanggal = 8
+print(tanggal == 8 or tanggal == 10)
+
+aktif = True
+print(not aktif)
+
+print("\n=========================================")
+print("OPERATOR STRING")
+print("=========================================")
+
+namadepan = "Bunga"
+namabelakang = "Agustina"
+namalengkap = namadepan + " " + namabelakang
+print(namalengkap)
+
+kata = "Cantik"
+print(kata * 7)
+
+kalimat = "Bunga Agustina adalah perempuan paling cantik yang pernah opal temui"
+print("Bunga" in kalimat)           #True
+print("Suka Marah" in kalimat)      #False
+print("paling cantik" in kalimat)   #True
